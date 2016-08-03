abba8fbc31c47f32bffe97dc7f3036a3ac890dca
Mike Lang <mlang@student.neumont.edu>
2016-08-03 12:57:20 -0600

    Made the pieces. All that's left for them is probably the constructors
    Signed-off-by: Mike Lang <mlang@student.neumont.edu>

---
+++ Backend/ChessPiece.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public enum TeamColor { WHITE, BLACK }
+
+    public class ChessPiece
+    {
+        public TeamColor Color { get; private set; }
+    }
+}
